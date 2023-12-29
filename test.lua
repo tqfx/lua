3c0510a3ec96dc76ff3b9b583551b4dfc5aa90c4
tqfx <tqfx@tqfx.org>
2023-12-30 00:05:18 +0800

    2023-12-30 00:05:18

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
-s = { { 1, 2 }, { 3, 4 }, ['1'] = { 5, 6 }, ['1\''] = { 7, 8 }, ['\'1'] = { 9, 10 } }
-s1 = { { 1, 2 }, { 3, 4 }, ['1'] = { 5, 6 }, ['1\''] = { 7, 8 }, ['\'1'] = { 9, 10 } }
+s = { { 1, 2 }, { 3, 4 }, ["'1"] = { 5, 6 }, ["\"1"] = { 7, 8 }, ["_1"] = { 9, 10 } }
+s1 = { { 1, 2 }, { 3, 4 }, ["'1"] = { 5, 6 }, ["\"1"] = { 7, 8 }, ["_1"] = { 9, 10 } }
 t = setmetatable({ func = function(self) return self end }, { __index = s })
 t1 = setmetatable({ func = function(self) return self end }, s1)
 s1.__newindex = function(self, key, val)
